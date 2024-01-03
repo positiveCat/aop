912a8b01ce5d1d5b3efd153e072aa9e4494164b4
seo <seo@seo.local>
2024-01-04 00:58:45 +0900

    Initial commit

---
+++ src/main/java/com/example/aop/dto/User.java
@@ -0,0 +1,43 @@
+package com.example.aop.dto;
+
+public class User {
+
+    private String id;
+
+    private String pwd;
+
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
